4b259585864b29d30fb504d6533a62ff1a9953b9
George Semerikov <kdog2105@mail.ru>
2026-02-13 09:11:28 +0300

    add consts to config

--- main/components/config.h
+++ main/components/config.h
@@ -7,8 +7,8 @@
 #define LED_COUNT   3
 
 // Пины для кнопок
-#define TOUCH_SENSOR_1_GPIO   GPIO_NUM_42  // Кнопка увеличения яркости
-#define TOUCH_SENSOR_2_GPIO   GPIO_NUM_41   // Кнопка уменьшения яркости
+#define TOUCH_SENSOR_1_GPIO   42//GPIO_NUM_42  // Кнопка увеличения яркости
+#define TOUCH_SENSOR_2_GPIO   41//GPIO_NUM_41   // Кнопка уменьшения яркости
 
 // Дребезг в миллисекундах
 #define DEBOUNCE_TIME_MS     50
@@ -20,6 +20,8 @@
 #define I2S_WS_GPIO         3//38
 #define I2S_DATA_OUT_GPIO   2//37
 #define AUDIO_AMPLITUDE     20000
+#define MAX_VOLUME          64
+#define MIN_VOLUME         -64
 
 // Начальная яркость
 #define INITIAL_BRIGHTNESS   50
